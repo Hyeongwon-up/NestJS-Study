9628a39acd7fe411942aaf7d248b566f723a00c0
Hyeongwon-up <leehwdatabase@gmail.com>
2022-03-20 18:10:46 +0900

    feat: PartialType 구성 적용

--- src/movies/dto/create-movie.dto.ts
+++ src/movies/dto/create-movie.dto.ts
@@ -1,9 +1,12 @@
-import { IsNumber, IsString } from "class-validator";
+import { IsNumber, IsOptional, IsString } from "class-validator";
 export class CreateMovieDto {
     @IsString()
     readonly title: string;
+    
     @IsNumber()
     readonly year: number;
+
     @IsString({ each: true })
+    @IsOptional()
     readonly genres: string[];
 }
